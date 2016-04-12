f906c4947250648e24bfdf2e199c6684c37691b6
Xu.hel <xu.hel@outlook.com>
2016-04-12 11:20:52 +0800

    建立 shell 命令执行。
    Signed-off-by: Xu.hel <xu.hel@outlook.com>

--- service.js
+++ service.js
@@ -1,5 +1,14 @@
 //Wingman-Pi.
 var io=require('socket.io')(8088);
+exec=require('child_process').exec;
 io.on('connection',function(socket){
-  
+  socket.on('shell',function(data){
+    console.log(data);
+    child=exec(data.shell,(error,stdout,stderr)=>{
+      console.log('stdout: ${stdout}');
+      socket.emit('shell',{out:stdout});
+      console.log('stderr: ${stderr}');
+      if(error!==null){console.log(`exec error: ${error}`);}
+    });
+  });
 });
