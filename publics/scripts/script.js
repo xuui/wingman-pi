37589654cc90cb23f3b3d6b92956106f0d63732f
Xu.hel <xu.hel@outlook.com>
2016-04-13 13:17:57 +0800

    加入 图片 的 socket 预览
    Signed-off-by: Xu.hel <xu.hel@outlook.com>

--- publics/scripts/script.js
+++ publics/scripts/script.js
@@ -2,6 +2,11 @@
 var socket=io();
 
 socket.on('connect',function(){
+  socket.on('image',function(data){
+    //console.log(data);
+    imgPreview.innerHTML='<img src="'+data.image+'" alt="'+data.file+'"/>';
+  });
+  
   socket.on('shell',function(data){
     console.log(data.out);
     imgPreview.innerHTML='<pre>'+data.out+'</pre>';
@@ -12,3 +17,37 @@ socket.on('connect',function(){
     socket.emit('shell',{shell:'dir /w'});
   });
 });
+
+/* Socket end */
+
+window.onload=function(){
+  var upFiles=document.querySelector('#upFiles');
+  var imgPreview=document.querySelector('#imgPreview');
+  if(typeof(FileReader)==='undefined'){
+    result.innerHTML='抱歉，你的浏览器不支持 FileReader，请使用现代浏览器操作！';
+    upFiles.setAttribute('disabled','disabled');
+  }else{
+    upFiles.addEventListener('change',readFile,false);
+  }
+}
+function readFile(){
+  //var file=this.files[0];
+  var files=this.files;
+  imgPreview.innerHTML='';
+  for(var i=0,l=files.length;i<l;i++){
+    //console.log(files[i]);
+    if(!/image\/\w+/.test(files[i].type)){
+      imgPreview.innerHTML='只提供图片文件的预览';
+      return false;
+    }
+    var filename=this.files[i].name;
+    console.log(filename);
+    var reader=new FileReader();
+    reader.readAsDataURL(files[i]);
+    reader.onload=function(e){
+      imgPreview.innerHTML+='<img src="'+this.result+'" alt=""/>';
+      //socket.emit('new image',this.result); //发送给socket;
+      socket.emit('image',{file:filename,image:this.result});
+    }
+  }
+}
--- service.js
+++ service.js
@@ -3,17 +3,21 @@ var express=require('express');
 var app=express();
 var server=require('http').createServer(app);
 var io=require('socket.io')(server);
-//var port=process.env.PORT || 8483;
-var port=process.env.PORT || 80;
+var port=process.env.PORT || 8483;
+//var port=process.env.PORT || 80;
+
 //var io=require('socket.io')(8088);
 var exec=require('child_process').exec;
 
 server.listen(port,function(){
-  console.log('listening at port %d',port);
+  console.log('Wingman-Pi listening at port %d',port);
 });
 app.use(express.static(__dirname+'/publics'));
 
 io.on('connection',function(socket){
+  socket.on('image',function(data){
+    socket.broadcast.emit('image',{file:data.file,image:data.image});
+  });
   socket.on('shell',function(data){
     console.log(data);
     child=exec(data.shell,{encoding:'utf8'},(error,stdout,stderr)=>{
