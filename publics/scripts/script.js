a28be7ec17245e00fa70060b3540c3cf1cc6bef0
Xu.hel <xu.hel@outlook.com>
2016-04-13 13:36:25 +0800

    shell 变更为 Terminal
    Signed-off-by: Xu.hel <xu.hel@outlook.com>

--- publics/scripts/script.js
+++ publics/scripts/script.js
@@ -1,23 +1,19 @@
 //var socket=io.connect('http://10.0.117.33:8088');
 var socket=io();
-
 socket.on('connect',function(){
   socket.on('image',function(data){
-    //console.log(data);
     imgPreview.innerHTML='<img src="'+data.image+'" alt="'+data.file+'"/>';
   });
-  
-  socket.on('shell',function(data){
+  socket.on('Terminal',function(data){
     console.log(data.out);
     imgPreview.innerHTML='<pre>'+data.out+'</pre>';
   });
   socket.on('news',function(data){
     console.log(data);
     socket.emit('event',{my:'data'});
-    socket.emit('shell',{shell:'dir /w'});
+    socket.emit('Terminal',{shell:'ls -l'});
   });
 });
-
 /* Socket end */
 
 window.onload=function(){
--- service.js
+++ service.js
@@ -18,11 +18,11 @@ io.on('connection',function(socket){
   socket.on('image',function(data){
     socket.broadcast.emit('image',{file:data.file,image:data.image});
   });
-  socket.on('shell',function(data){
+  socket.on('Terminal',function(data){
     console.log(data);
     child=exec(data.shell,{encoding:'utf8'},(error,stdout,stderr)=>{
       console.log('stdout: ${stdout}');
-      socket.emit('shell',{out:stdout});
+      socket.emit('Terminal',{out:stdout});
       console.log('stderr: ${stderr}');
       if(error!==null){console.log(`exec error: ${error}`);}
     });
